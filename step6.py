9f75272843acfa3467991171f920d98897aab08b
Kıvanç Güçkıran <kivancguckiran@gmail.com>
2018-03-24 15:54:07 +0300

    improvements
    applied median and gabor kernels width height increased

--- step6.py
+++ step6.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utils import openImageFile, saveImageFile, applyOpening, applyClosing, applyGaussian, applySegmentation, applyOrientation, applyFrequency, applyGaborFilter, applySkeletonization
+from utils import openImageFile, saveImageFile, applyOpening, applyClosing, applyGaussian, applyMedian, applySegmentation, applyOrientation, applyFrequency, applyGaborFilter, applySkeletonization
 from skimage.morphology import skeletonize, disk
 from skimage.util import invert
 
@@ -7,12 +7,13 @@ image = openImageFile('fingerprint.bmp')
 
 image = applyClosing(image, disk(1))
 image = applyOpening(image, disk(1))
+image = applyMedian(image, disk(1))
 image = applyGaussian(image)
 
 # Assigned block size, which is divisive of 288 and 384
 normImage, mask = applySegmentation(image, 12, 0.1)
 orientImage = applyOrientation(normImage, 7, 1, 42, 7)
 freqImage, meanFreq = applyFrequency(normImage, mask, orientImage, 24, 5, 5, 15)
-gaborKernel, gaboredImage = applyGaborFilter(normImage, orientImage, meanFreq * mask, 0.5, 0.5)
+gaborKernel, gaboredImage = applyGaborFilter(normImage, orientImage, meanFreq * mask, 0.75, 0.75)
 thinnedImage = np.asarray(invert(skeletonize(gaboredImage)), dtype=int)
 saveImageFile(thinnedImage, 'step6/skel.bmp')
